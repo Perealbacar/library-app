a6178f77bb09419d7f65d8ae0ddf8cebc682ad07
Perealbacar <37050221+Perealbacar@users.noreply.github.com>
2024-10-16 17:29:12 +0200

    Added addBookToLibrary function and myLibrary

--- library.js
+++ library.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author,pages,read) {
     this.title = title;
     this.author = author;
@@ -8,11 +10,15 @@ function Book(title, author,pages,read) {
         return (this.title + " by " + this.author + ", " + this.pages + " pages," + this.read)
     }
    // theHobbit.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
-
+   
 };
+
+function addBookToLibrary(book){
+    myLibrary.push(book);
+}
 
 const Tolkien = new Book("The Hobbit", "J.R.R. Tolkien", "295", false);
 
-function CallBook(book){
+function callBook(book){
     console.log(book.info());
 }
