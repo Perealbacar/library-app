9efc64b081f56fb3ac2f2f8bc7cfd8464459f389
Perealbacar <37050221+Perealbacar@users.noreply.github.com>
2024-10-16 17:57:17 +0200

    Added functions to add and list books to myLibrary

--- library.js
+++ library.js
@@ -10,7 +10,7 @@ function Book(title, author,pages,read) {
         return (this.title + " by " + this.author + ", " + this.pages + " pages," + this.read)
     }
    // theHobbit.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
-   
+
 };
 
 function addBookToLibrary(book){
@@ -18,7 +18,22 @@ function addBookToLibrary(book){
 }
 
 const Tolkien = new Book("The Hobbit", "J.R.R. Tolkien", "295", false);
+const Tolkien2 = new Book("The Hobbit2", "J.R.R. Tolkien", "295", false);
+const Tolkien3 = new Book("The Hobbit3", "J.R.R. Tolkien", "295", false);
+const Tolkien4 = new Book("The Hobbit4", "J.R.R. Tolkien", "295", false);
 
 function callBook(book){
     console.log(book.info());
+}
+
+function addAllBooks(){
+    myLibrary.push(Tolkien,Tolkien2,Tolkien3,Tolkien4)
+}
+
+function listAllBooks(){
+    let i = 0;
+    while(i < myLibrary.length){
+        callBook(myLibrary[i]);
+        i++;
+    }
 }
